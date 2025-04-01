387a725d1437dbfa07d5ec73787fe04b7b8e80de
Ymir-boop <karl.ymir@gmail.com>
2025-04-01 10:04:41 +0000

    setti upp teninga klasann

---
+++ src/main/java/Vinnsla/Dice.java
@@ -0,0 +1,33 @@
+package Vinnsla;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Dice {
+    private static final SimpleIntegerProperty rollResult = new SimpleIntegerProperty();
+    private static final int MAX = 6;
+
+    /**
+     * @return a random number from 1 to 6
+     */
+    public void roll() {
+        rollResult.set((int) (Math.random() * MAX) + 1);
+    }
+
+
+    /**
+     * @return the value of kast
+     */
+    public static int getRollResult() {
+        return rollResult.get();
+    }
+
+    /**
+     * @param args ónotað
+     */
+    public static void main (String[] args) {
+        Dice t = new Dice();
+        t.roll();
+        System.out.println(t.getRollResult());
+    }
+
+}
