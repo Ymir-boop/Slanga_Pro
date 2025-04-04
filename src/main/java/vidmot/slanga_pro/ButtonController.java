29a7a94bf678c2990f9f9890067a250162a56f1f
Ymir-boop <karl.ymir@gmail.com>
2025-04-04 15:11:40 +0000

    vann í takka svæðinu

--- src/main/java/vidmot/slanga_pro/ButtonController.java
+++ src/main/java/vidmot/slanga_pro/ButtonController.java
@@ -1,4 +1,11 @@
 package vidmot.slanga_pro;
 
+import javafx.scene.input.MouseEvent;
+
 public class ButtonController {
+
+    public void diceHandler(MouseEvent mouseEvent) {
+
+    }
+    // þessi klasi stýrir tökkunum í leiknum og labels og mynd og progress bar og hvaðeina
 }
